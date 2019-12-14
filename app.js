a4505b7d42ce2008ddf47bc92adb311fc1cd19a0
kie-13 <57992103+kie-13@users.noreply.github.com>
2019-12-14 01:50:36 +0100

    commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('Funcionando otraa vez')
+class Producto{
+    constructor(nombre,precio,año){
+    this.nombre=nombre;
+    this.precio=precio;
+    this.año=año;
+    }
+};
+
+const producto=new Producto('hola',10,1999);
+alert(producto);
